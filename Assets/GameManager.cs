96951f42b8b9d5ecdb55b9718cb351a07b40d1bc
Senexis <timoviveen@live.nl>
2019-06-14 16:30:59 +0200

    Initial project upload.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public InputManager inputManager;
+
+    private int scorePlayer = 0;
+    private int scoreComputer = 0;
+    private RpsType computerRpsType;
+
+    public void TryScore(RpsType type)
+    {
+        inputManager.DisplayPlayerRpsImage(type);
+
+        var computerRps = ChooseNewComputerRpsType();
+
+        if (type == RpsType.Paper)
+        {
+            if (computerRps == RpsType.Rock)
+            {
+                scorePlayer++;
+            }
+            else if (computerRps == RpsType.Scissors)
+            {
+                scoreComputer++;
+            }
+        }
+
+        if (type == RpsType.Rock)
+        {
+            if (computerRps == RpsType.Scissors)
+            {
+                scorePlayer++;
+            }
+            else if (computerRps == RpsType.Paper)
+            {
+                scoreComputer++;
+            }
+        }
+
+        if (type == RpsType.Scissors)
+        {
+            if (computerRps == RpsType.Paper)
+            {
+                scorePlayer++;
+            }
+            else if (computerRps == RpsType.Rock)
+            {
+                scoreComputer++;
+            }
+        }
+
+        inputManager.UpdateScore(scorePlayer, scoreComputer);
+    }
+
+    public RpsType ChooseNewComputerRpsType()
+    {
+        int randomValue = Random.Range(0, 3);
+
+        switch (randomValue)
+        {
+            case 0:
+                computerRpsType = RpsType.Rock;
+                break;
+            case 1:
+                computerRpsType = RpsType.Paper;
+                break;
+            case 2:
+                computerRpsType = RpsType.Scissors;
+                break;
+        }
+
+        inputManager.DisplayComputerRpsImage(computerRpsType);
+        return computerRpsType;
+    }
+
+    public enum RpsType
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Image computerSprite;
+    public Image playerSprite;
+    public GameManager gameManager;
+
+    public void UpdateScore(int playerScore, int computerScore)
+    {
+        scoreText.text = $"{playerScore} - {computerScore}";
+    }
+
+    internal void DisplayComputerRpsImage(GameManager.RpsType computerRpsType)
+    {
+        computerSprite.sprite = Resources.Load<Sprite>(computerRpsType.ToString());
+    }
+
+    internal void DisplayPlayerRpsImage(GameManager.RpsType playerRpsType)
+    {
+        playerSprite.sprite = Resources.Load<Sprite>(playerRpsType.ToString());
+    }
+
+    public void RockButtonClick()
+    {
+        gameManager.TryScore(GameManager.RpsType.Rock);
+    }
+
+    public void PaperButtonClick()
+    {
+        gameManager.TryScore(GameManager.RpsType.Paper);
+    }
+
+    public void ScissorsButtonClick()
+    {
+        gameManager.TryScore(GameManager.RpsType.Scissors);
+    }
+}
